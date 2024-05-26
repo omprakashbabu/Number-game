ec45ad3c12dd2fce4a3dbb953c7d550d4382c986
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2024-05-26 13:34:57 +0530

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main()
     cin>>ui;
     if(ui == 'y')
     {
-        a+=1;
+        a+=1 ;
         cout<<"Check if your number belongs to this [2,3,6,7,10,11,14,15]"<<endl;
         cout<<"If yes type 'y'. If no type 'n'"<<endl;
         cin>>ui;
